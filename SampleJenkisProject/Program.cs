aad17df80269350208ccb7990a8ecb9cd11ef80b
Cetin, Fatih (CT RDA DS EU TR CA CRANES1) <fatihcetin@siemens.com>
2019-09-25 11:52:41 +0300

    Hello world message changed

--- SampleJenkisProject/Program.cs
+++ SampleJenkisProject/Program.cs
@@ -10,7 +10,7 @@ namespace SampleJenkisProject
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello World");
+            Console.Write("Hello World changed");
         }
     }
 }
